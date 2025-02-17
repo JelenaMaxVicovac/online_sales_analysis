2372b66175e1a660d9c7c25ed60268b948200acf
JelenaMaxVicovac <maksimovic84@live.com>
2025-02-17 15:52:40 +0100

    Obrisan prikaz proizvoda i ukupna vrednost inventara u main failu radeci u main grani

--- main.py
+++ main.py
@@ -5,17 +5,12 @@ import product_manager
 manager=product_manager.ProductManager()
 
 #kreiranje nekoliko objekata klase Produkt
-pr1=product.Product("maramice",250,10)
+pr1=product.Product("cokolada",250,35)
 pr2=product.Product("smoki",75,15)
-pr3=product.Product("zvake",135,12)
+pr3=product.Product("mleko",135,20)
 
 #dodavanje objekata u listu i prikaz iste
 manager.input_product(pr1)
 manager.input_product(pr2)
 manager.input_product(pr3)
 
-#prikaz proizvoda
-manager.display_products()
-
-#prikaz ukupne vrednosti inventara
-manager.total_value()
